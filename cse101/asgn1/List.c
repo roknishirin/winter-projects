ab154bc3f855adfa2e8e0b567fa965b93a3e75ff
ShirinRokni <roknishirin@gmail.com>
2023-01-02 13:54:53 -0800

    changes to listobj struct

--- cse101/asgn1/List.c
+++ cse101/asgn1/List.c
@@ -3,9 +3,11 @@
 
 // Creates and returns a new empty List.
 typedef struct ListObj {
-    int pos; 
-    int length;
-    
+    int pos; // the position of the cursor 
+    int length; // length
+    struct Node *cursor; // a pointer to the cursor
+    struct Node *start; // a pointer to the first position
+    struct Node *end; // a pointer to the last position 
 
 } ListObj;
 
