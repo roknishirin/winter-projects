4b13f67b05e7da1ba56755d374c2560b8ca86bc2
ShirinRokni <roknishirin@gmail.com>
2023-01-02 15:44:50 -0800

    adding list.c changes

--- cse101/asgn1/List.c
+++ cse101/asgn1/List.c
@@ -14,12 +14,31 @@ typedef struct ListObj {
 
 } ListObj;
 
-// did you make a node object? 
-typdef struct NodeObj {
-    int info;
-    Node next;
-    Node prev; 
-    
+typedef struct NodeObj {
+    int data;
+    Node next; // next node
+    Node prev;  // previous node
+
 } NodeObj;
 
 // Constructors-Destructors -----------------------------------------
+// Creates and returns a new empty List.
+List newList(void) {
+    List L = malloc(sizeof(ListObj));
+    L->front = NULL;
+    L->back = NULL;
+    L->length = 0;
+    L->index = -1;
+    L->cursor = NULL;
+}
+
+// Frees all heap memory associated with *pL, and sets *pL to NULL.
+void freeList(List* pL) {
+    if (*pL != NULL && pL != NULL) {
+        while (length(*pL) != 0) {
+            clear(*pL);
+        }
+    }
+    free(*pL);
+    *pL = NULL;
+}
