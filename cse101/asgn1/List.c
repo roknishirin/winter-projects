8a2d39b4c2cc484207711392099805de6ed9cd65
ShirinRokni <roknishirin@gmail.com>
2023-01-02 14:03:09 -0800

    adding nodeobj

--- cse101/asgn1/List.c
+++ cse101/asgn1/List.c
@@ -1,6 +1,9 @@
 #include "List.h"
 #include <stdio.h>
 
+
+// structs  ---------------------------------------------------------
+
 // Creates and returns a new empty List.
 typedef struct ListObj {
     int pos; // the position of the cursor 
@@ -11,3 +14,12 @@ typedef struct ListObj {
 
 } ListObj;
 
+// did you make a node object? 
+typdef struct NodeObj {
+    int info;
+    Node next;
+    Node prev; 
+    
+} NodeObj;
+
+// Constructors-Destructors -----------------------------------------
