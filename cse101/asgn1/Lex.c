11d76afa5a575e4e0898a23917a7c50b9c89f4bc
ShirinRokni <roknishirin@gmail.com>
2023-01-03 17:17:48 -0800

    adding lex

---
+++ cse101/asgn1/Lex.c
@@ -0,0 +1,96 @@
+#include "List.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// used FileIO.c
+
+#define MAX_LEN 300
+
+int main(int argc, char *argv[]) {
+    FILE *in, *out;
+
+    // check command line for correct number of arguments
+    if (argc != 3) {
+        printf("Usage: %s <input file> <output file>\n", argv[0]);
+        exit(1);
+    }
+
+    // open files for reading and writing
+    in = fopen(argv[1], "r");
+    if (in == NULL) {
+        printf("Unable to open file %s for reading\n", argv[1]);
+        exit(1);
+    }
+
+    out = fopen(argv[2], "w");
+    if (out == NULL) {
+        printf("Unable to open file %s for writing\n", argv[2]);
+        exit(1);
+    }
+
+    int line_count = 0;
+    while (fgets(line, MAX_LEN, in) != NULL) {
+        line_count++;
+    }
+
+    // allocating array
+    char **arry = malloc(linecount * sizeof(char *));
+
+    fseek(in, 0, SEEK_SET); // to read the start of the input file
+
+    /* need to change ? */
+    for (int i = 0; i < line_count; i++) {
+        arry[i] = malloc(268 * sizeof(char));
+    }
+
+    /* CHANGE PROBABLY*/
+    int index = 0;
+    while (fgets(line, MAX_LEN, in) != NULL) {
+        strcpy(array[index], line);
+        index++;
+    }
+
+    // creating the list
+    List lst = newList();
+    for (int i = 0; i < line_count; i++) {
+        if (length(lst) == 0) {
+            append(lst, i);
+        }
+        else {
+            if (strcmp(arry[get(lst)]), arry[i] >= 0) {
+                prepend(lst, i);
+            }
+            else {
+                while (index(lst) >= 0) {
+                    if (strcmp(arry[get(lst)]), arry[i] >= 0) {
+                        insertBefore(lst, i);
+                        break;
+                    }
+                    moveNext;
+                }
+                if (index(lst) == -1) {
+                    append(lst, i);
+                }
+            }
+        }
+        moveFront(lst);
+    }
+
+    // printing
+    for (int i = 0; i < line_count; i++) {
+        fprintf(out, "%s", arry[get(L)]);
+        moveNext(lst);
+    }
+
+    // freeing 
+    for (int i = 0; i < line_count; i++) {
+        free(arry[i]);
+    }
+
+    free(arry);
+    freeList(lst);
+    fclose(in);
+    fclose(out);
+    return 0;
+}
