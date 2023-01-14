722afc34645f7df02515e7a40bdb61a900e67bcc
ShirinRokni <roknishirin@gmail.com>
2023-01-14 12:56:56 -0800

    adding prog1.py

--- cse107/prog1.py
+++ cse107/prog1.py
@@ -1,7 +1,7 @@
 """*******************************************
 	python code for project 1 in
 	CSE 107 in 2022 winter, UC Santa Cruz,
-			for Prof. Chen Qian.
+			for Profw. Chen Qian.
 **********************************************
 	Student name: Shirin Rokni
 	UCSC email: shrokni@ucsc.edu
@@ -27,13 +27,46 @@ Hint(s):
 """
 
 def distribution_random():
-    pass
+    np.random.seed(0)
+    x_vals = np.arange(10000)
+    y_vals = np.random.random(10000)
+    plt.scatter(x_vals, y_vals, c = '#F8B195', s = 2)
+    plt.xlabel("random number")
+    plt.ylabel("index")
+    plt.title("distribution_random")
+    plt.savefig("plot1.png")
+    plt.show()
 
 def distribution_KISS():
-    pass
+    rng_kiss = sri.KISS(123958, 34987243, 3495825239, 2398172431)
+    x_vals = np.arange(10000)
+    y_vals = []
+    for i in range(10000): 
+        y_vals.append(next(rng_kiss) % 10000)
+
+    plt.scatter(x_vals, y_vals, c = '#F67280', s = 2)
+    plt.xlabel("random number")
+    plt.ylabel("index")
+    plt.title("distribution_KISS")
+    plt.savefig("plot2.png")
+    plt.show()
 
 def distribution_SHR3():
-    pass
+    rng_shr3 = sri.SHR3(3360276411)
+    x_vals = np.arange(10000)
+    y_vals = []
+    for i in range(10000):
+        y_vals.append(next(rng_shr3) % 10000)
+
+    plt.scatter(x_vals, y_vals, c = '#C06C84', s = 2)
+    plt.xlabel("random number")
+    plt.ylabel("index")
+    plt.title("distribution_SHR3")
+    plt.savefig("plot3.png")
+    plt.show()
+    
+
+
 
 def pingpong(n:int=21, p:float=0.3, ntrials:int=5000, seed:int=0):
     """
@@ -48,13 +81,35 @@ def pingpong(n:int=21, p:float=0.3, ntrials:int=5000, seed:int=0):
     You can ONLY use the function np.random.random() to generate randomness; 
     this function generates a random float from the interval [0, 1).
     """
+    np.random.seed(seed)
+    wins = 0
 
     def sim_one_game():
         #     """
         #     This is a nested function only accessible by parent sim_prob,
         #     which we're in now. You may want to implement this function!
         #     """
-        pass # TODO: Your code here (10-20 lines)
+        # TODO: Your code here (10-20 lines)
+        me = 0
+        them = 0
+
+        while True: 
+            output = np.random.random()
+            if output <= p:
+                me += 1
+            else: 
+                them += 1
+            
+            if (me >= n) and ((me - them) >= 2):
+                return True
+            if (them >= n) and ((them - me) >= 2): 
+                return False
+        
+    for i in range(ntrials):
+        if sim_one_game():
+            wins += 1
+
+    return wins / ntrials
 
 
 def plot_output():
@@ -78,9 +133,33 @@ def plot_output():
     :return: Nothing. Just save the plot you made!
     """
     
-    pass # TODO: Your code here (10-20 lines)
+    # TODO: Your code here (10-20 lines)
+    index = 0
+    p = np.linspace(0.0, 1.0, 26) # a way of making a list from 0 to 1.0
+    n_3 = np.zeros(26)
+    n_11 = np.zeros(26)
+    n_21 = np.zeros(26)
 
+    for i in range(26): 
+        n_3[i] = pingpong(3, index)
+        n_11[i] = pingpong(11, index)
+        n_21[i] = pingpong(21, index)
+        index += 0.04
+
+    plt.plot(p, n_3, color= '#FFCCFF', label = 'n = 3')
+    plt.plot(p, n_11, color= '#FFCC66', label = 'n = 11', linestyle='--')
+    plt.plot(p, n_21, color= '#FF99CC', label = 'n = 21',linestyle=':')
+    plt.title("Relating P(win point) to P(win game)")
+    plt.legend(loc="upper left")
+    plt.xlabel("P(win point)")
+    plt.ylabel("P(win game)")
+    plt.savefig("pingpong.png")
+    plt.show()
 
 if __name__ == '__main__':
     # You can test out things here. Feel free to write anything below.
-    pass
+    distribution_random()
+    distribution_KISS()
+    distribution_SHR3()
+    pingpong()
+    plot_output()
